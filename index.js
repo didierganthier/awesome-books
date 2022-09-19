dffec4fa40344568238a1afddcd42b0e4c69fad3
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-09-19 13:42:50 -0400

    Fix linters errors

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ let booksObject = [];
 
 // Pouplate the HTML with the books
 const populateHTML = (books) => {
-    const booksContainer = document.createElement('div');
-    booksContainer.innerHTML = '';
-    books.forEach(book => {
-        booksContainer.innerHTML += `
+  const booksContainer = document.createElement('div');
+  booksContainer.innerHTML = '';
+  books.forEach((book) => {
+    booksContainer.innerHTML += `
             <div class="book" id="book${book.id}">
                 <p class="title">${book.title}</p>
                 <p class="author">${book.author}</p>
@@ -17,41 +17,40 @@ const populateHTML = (books) => {
                 <hr/>
             </div>
             `;
-    });
-    document.querySelector('.books').appendChild(booksContainer);
-}
+  });
+  document.querySelector('.books').appendChild(booksContainer);
+};
 
 populateHTML(booksObject);
 
 // Add event listener to button
-document.getElementsByClassName('add-books')[0].addEventListener('click', function() {
-    booksObject.push({
-        title: titleInput.value,
-        author: authorInput.value,
-        id: booksObject.length + 1,
-    });
-    // Clear the input fields
-    titleInput.value = '';
-    authorInput.value = '';
+document.getElementsByClassName('add-books')[0].addEventListener('click', () => {
+  booksObject.push({
+    title: titleInput.value,
+    author: authorInput.value,
+    id: booksObject.length + 1,
+  });
+  // Clear the input fields
+  titleInput.value = '';
+  authorInput.value = '';
 
-    // Add books object to local storage
-    localStorage.setItem('books', JSON.stringify(booksObject));
+  // Add books object to local storage
+  localStorage.setItem('books', JSON.stringify(booksObject));
 
-    // Clear the books container
-    document.querySelector('.books').innerHTML = '';
-    // Populate the HTML with the new books
-    populateHTML(booksObject);
+  // Clear the books container
+  document.querySelector('.books').innerHTML = '';
+  // Populate the HTML with the new books
+  populateHTML(booksObject);
 });
 
 // Add event listener to each remove button
-document.getElementsByClassName('books')[0].addEventListener('click', function(e) {
-    if (e.target.classList.contains('remove-btn')) {
-        const book = e.target.parentElement;
-        book.remove();
-        // Remove the book from the books object
-        booksObject = booksObject.filter(b => b.title !== book.querySelector('.title').textContent);
-        // Add books object to local storage
-        localStorage.setItem('books', JSON.stringify(booksObject));
-    }
+document.getElementsByClassName('books')[0].addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-btn')) {
+    const book = e.target.parentElement;
+    book.remove();
+    // Remove the book from the books object
+    booksObject = booksObject.filter((b) => b.title !== book.querySelector('.title').textContent);
+    // Add books object to local storage
+    localStorage.setItem('books', JSON.stringify(booksObject));
+  }
 });
-
