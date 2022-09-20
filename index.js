618bf3ff1c40182005bb737526509e8fcd8d53f5
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-09-20 13:36:20 -0400

    Add different backgrounds

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 class Book {
-  constructor(title, author, add, remove) {
+  constructor(title, author) {
     this.title = title;
     this.author = author;
-    this.add = add;
-    this.remove = remove;
   }
 
   addBook() {
@@ -25,18 +23,33 @@ const authorInput = document.querySelector('.author-value');
 const displayBooks = () => {
   const books = JSON.parse(localStorage.getItem('books')) || [];
   const booksContainer = document.querySelector('.books');
-  booksContainer.innerHTML = '';
-  books.forEach((book) => {
-    booksContainer.innerHTML += `
+
+  if (books.length === 0) {
+    booksContainer.innerHTML = '<p class="no-books">No books added yet</p>';
+    // Remove border from books container
+    booksContainer.style.border = 'none';
+  } else {
+    booksContainer.style.border = '2px solid #000';
+    booksContainer.innerHTML = '';
+    books.forEach((book, index) => {
+      booksContainer.innerHTML += `
       <div class="book">
         <div class="data-container">
           <p class="title">${book.title}</p>
+          <span>by</span>
           <p class="author">${book.author}</p>
         </div>
         <button type="button" class="remove-btn">Remove</button>
       </div>
     `;
-  });
+      // Different background for odd and even books
+      if (index % 2 === 0) {
+        booksContainer.lastElementChild.style.backgroundColor = '#DDD';
+      } else {
+        booksContainer.lastElementChild.style.backgroundColor = '#fff';
+      }
+    });
+  }
 };
 
 const addBook = () => {
@@ -53,6 +66,12 @@ const removeBook = (e) => {
     const bookToRemove = new Book(book.querySelector('.title').textContent);
     bookToRemove.removeBook();
     book.remove();
+    // Remove border from books container if no books left
+    if (document.querySelector('.books').children.length === 0) {
+      document.querySelector('.books').style.border = 'none';
+      // Add no books message
+      document.querySelector('.books').innerHTML = '<p class="no-books">No books added yet</p>';
+    }
   }
 };
 
